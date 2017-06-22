d6c875dd3ad9b081b2c3f112b96beb4d4b118b41
Leo_Gu <gffsnake2012@163.com>
2017-06-22 18:44:31 +0800

    1

--- TestGit/TestGit/Program.cs
+++ TestGit/TestGit/Program.cs
@@ -11,6 +11,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Master");
+            Console.WriteLine("111");
             Console.ReadKey();
         }
     }
